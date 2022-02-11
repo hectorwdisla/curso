ca9ba1e65fcbe6d998b53a9001697ddf39ada65f
Hector W. Disla <hectorwdisla@gmail.com>
2022-02-11 08:22:52 -0400

    Inicio

---
+++ academy/__manifest__.py
@@ -0,0 +1,18 @@
+# .*. coding: utf-8 .*.
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sesions
+        - Attendees
+        """,
+    'autohr':'Odoo',
+    'website': 'https://odoo.com',
+    'category':'Training',
+    'version':'0.1',
+    'depends':['base'],
+    'data':[],
+    'demo':[],
+}
